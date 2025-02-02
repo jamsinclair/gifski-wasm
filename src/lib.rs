085967a1af21df95423fef38473d6bdc41d0a445
Jamie Sinclair <jamsinclairnz@gmail.com>
2025-02-03 00:54:14 +0900

    update lib.rs with logic to support custom frame durations

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 #[cfg(feature = "parallel")]
 pub use wasm_bindgen_rayon::init_thread_pool;
 
@@ -5,13 +8,24 @@ pub use wasm_bindgen_rayon::init_thread_pool;
 extern crate console_error_panic_hook;
 use wasm_bindgen::prelude::*;
 
+use gifski_lite::*;
 use imgref::*;
 use rgb::*;
-use gifski_lite::*;
 use std::io::Cursor;
 
 #[wasm_bindgen]
-pub fn encode(frames: &[u8], num_of_frames: usize, width: u32, height: u32, fps: u8, quality: Option<u8>, repeat: Option<i32>,  resize_width: Option<u32>, resize_height: Option<u32>) -> Vec<u8> {
+pub fn encode(
+    frames: &[u8],
+    num_of_frames: usize,
+    width: u32,
+    height: u32,
+    fps: Option<u8>,
+    frame_durations: Option<Vec<u32>>,
+    quality: Option<u8>,
+    repeat: Option<i32>,
+    resize_width: Option<u32>,
+    resize_height: Option<u32>,
+) -> Vec<u8> {
     #[cfg(feature = "debug")]
     console_error_panic_hook::set_once();
 
@@ -23,12 +37,53 @@ pub fn encode(frames: &[u8], num_of_frames: usize, width: u32, height: u32, fps:
         height: resize_height,
         quality: quality.unwrap_or(80),
         fast: false, // TODO: Do we want to expose this? Does it have much of an impact?
-        repeat: if _repeat >= 0 { Repeat::Finite(_repeat as u16) } else { Repeat::Infinite },
+        repeat: if _repeat >= 0 {
+            Repeat::Finite(_repeat as u16)
+        } else {
+            Repeat::Infinite
+        },
     };
 
     if num_of_frames == 1 {
         panic!("Only a single image file was given as an input. This is not enough to make an animation.");
     }
+
+    if fps.is_none() && frame_durations.is_none() {
+        panic!("Either FPS or Frame Durations must be provided.");
+    }
+
+    if fps.is_some() && frame_durations.is_some() {
+        panic!("FPS and Frame Durations cannot be provided at the same time.");
+    }
+
+    let frame_timings: Vec<f64> = match frame_durations {
+        Some(durations) => {
+            if durations.len() != num_of_frames {
+                panic!(
+                    "The number of frame durations provided does not match the number of frames."
+                );
+            }
+            let mut timings = Vec::new();
+            let mut total_duration = 0.0;
+            for i in 0..num_of_frames {
+                if i == 0 {
+                    let duration = durations[num_of_frames - 1] as f64 / 1000.0;
+                    total_duration += duration
+                } else {
+                    total_duration += durations[i - 1] as f64 / 1000.0;
+                }
+                timings.push(total_duration)
+            }
+            timings
+        }
+        None => {
+            let fps = fps.unwrap();
+            let frame_duration = 1.0 / fps as f64;
+            (0..num_of_frames)
+                .map(|i| (i + 1) as f64 * frame_duration)
+                .collect()
+        }
+    };
 
     let frame_size = width * height * 4;
     if num_of_frames != frames.len() / frame_size as usize {
@@ -39,7 +94,9 @@ pub fn encode(frames: &[u8], num_of_frames: usize, width: u32, height: u32, fps:
 
     for (index, frame) in frames.chunks_exact(frame_size as usize).enumerate() {
         let image = ImgVec::new(frame.as_rgba().into(), width as usize, height as usize);
-        collector.add_frame_rgba(index, image, index as f64 / fps as f64).unwrap_throw();
+        collector
+            .add_frame_rgba(index, image, frame_timings[index])
+            .unwrap_throw();
     }
 
     drop(collector);
