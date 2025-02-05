a30171fe31671fd66f1fee4722cc1f4de67c29d6
Jamie Sinclair <jamsinclairnz@gmail.com>
2025-02-05 15:58:05 +0900

    refactor: make encode logic more re-usable and drop need for ImageData

--- src/encode-multi-thread.ts
+++ src/encode-multi-thread.ts
@@ -1,5 +1,6 @@
 import type { InitInput } from '../pkg/gifski_wasm.js';
 import { threads } from 'wasm-feature-detect';
+import { _internal_encode, EncodeOptions } from './encode';
 
 async function initMT(moduleOrPath?: InitInput) {
   const {
@@ -44,79 +45,9 @@ export async function init(
   return wasmReady;
 }
 
-function framesToBuffer(frames: Array<Uint8Array | ImageData>): Uint8Array {
-  // Pre-calculate the total length of all frames and instantiate a buffer of that size
-  // Faster than dynamically re-creating the buffer each time (previous approach)
-  const totalLength = frames.reduce((acc, frame) => {
-      const _frame = (frame instanceof ImageData || "data" in frame ? frame.data : frame) as Uint8Array;
-      return acc + _frame.length;
-  }, 0);
-  const framesBuffer = new Uint8Array(totalLength);
-
-  let offset = 0;
-  frames.forEach(frame => {
-      const _frame = (frame instanceof ImageData || "data" in frame ? frame.data : frame) as Uint8Array;
-      framesBuffer.set(_frame, offset);
-      offset += _frame.length;
-  });
-
-  return framesBuffer;
-}
-
-type BaseEncodeOptions = {
-  frames: Array<Uint8Array | ImageData>;
-  width: number;
-  height: number;
-  quality?: number;
-  repeat?: number;
-  resizeWidth?: number;
-  resizeHeight?: number;
-}
-
-type EncodeOptions = BaseEncodeOptions & {
-  fps: number;
-  frameDurations?: never;
-} | BaseEncodeOptions & {
-  fps?: never;
-  frameDurations: Array<number> | Uint32Array;
-}
-
-export async function encode({
-    frames,
-    width,
-    height,
-    fps,
-    frameDurations,
-    quality,
-    repeat,
-    resizeWidth,
-    resizeHeight,
-}: EncodeOptions) {
-  if (frames.length === 1) {
-    throw new Error('At least 2 frames are required to encode a GIF with gifski');
-  }
-
-  if (!fps && !frameDurations) {
-    throw new Error('Either fps or frameDurations must be provided');
-  }
-
-  if (fps && frameDurations) {
-    throw new Error('fps and frameDurations cannot be provided at the same time');
-  }
-
-  if (frameDurations && frameDurations.length !== frames.length) {
-    throw new Error('The number of frame durations must match the number of frames');
-  }
-
-  const { encode } = await init();
-
-  const numOfFrames = frames.length;
-  const framesBuffer = framesToBuffer(frames);
-  const _frameDurations = frameDurations ? new Uint32Array(frameDurations) : undefined;
-  const buffer = await encode(framesBuffer, numOfFrames, width, height, fps, _frameDurations, quality, repeat, resizeWidth, resizeHeight);
-  if (!buffer) throw new Error('Encoding error.');
-
-  return buffer;
+export async function encode(options: EncodeOptions): Promise<Uint8Array> {
+  const { encode: wasmEncode } = await init();
+  return _internal_encode(wasmEncode, options);
 }
 
 export default encode;
